a7831f6ae4864ca052add5dc3fb0d62cc9992db0
Shubhangi Gupta <shubhang@oradev.oraclecorp.com>
2017-04-11 23:26:04 +0530

    custom spinner

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 
-const appCode : string = `
+const appCode: string = `
 import { Component } from '@angular/core'
 
 @Component({
@@ -12,7 +12,7 @@ import { Component } from '@angular/core'
 
 export class AppComponent {
     showLoader : boolean;
-    
+
     fetchData() : void {
         this.showLoader = true;
         //Fetch your async data here
@@ -21,7 +21,7 @@ export class AppComponent {
 }
 `;
 
-const tableCode : string = `
+const tableCode: string = `
 import { Component, Input } from '@angular/core';
 
 @Component({
@@ -31,9 +31,9 @@ import { Component, Input } from '@angular/core';
 
 export class TableComponent {
     @Input() showLoader : boolean;
-}  
+}
 `;
-const tableHtml : string = `
+const tableHtml: string = `
 <!-- NOTE: For the componet to work , you need to wrap your
 component tempalate inside relative positioned div -->
 <div style="position : relative">
@@ -47,7 +47,89 @@ component tempalate inside relative positioned div -->
                 <th>Username</th>
             </tr>
         </thead>
-        <tbody>           
+        <tbody>
+            <tr *ngFor="#user of users">
+                <td>{{user.id}}
+                <td>{{user.firstname}}</td>
+                <td>{{user.lastname}}</td>
+                <td>{{user.username}}</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+`;
+const tableCode1: string = `
+import { Component, Input } from '@angular/core';
+@Component({
+    selector: 'table-component',
+    templateUrl: "./template/table.html",
+    styles: [.spinner - 2 {
+    width: 40px;height: 40px;
+
+position: relative;
+margin: auto;
+  }
+
+  .double - bounce1, .double - bounce2 {
+  width: 100 %;
+  height: 100 %;
+  border - radius: 50 %;
+  background - color: #333;
+  opacity: 0.6;
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  -webkit - animation: sk - bounce 2.0s infinite ease-in-out;
+  animation: sk - bounce 2.0s infinite ease-in-out;
+}
+
+  .double - bounce2 {
+  -webkit - animation - delay: -1.0s;
+  animation - delay: -1.0s;
+}
+
+@-webkit - keyframes sk- bounce {
+  0 %, 100 % { -webkit - transform: scale(0.0)
+}
+50 % { -webkit - transform: scale(1.0) }
+  }
+
+@keyframes sk - bounce {
+  0 %, 100 % {
+    transform: scale(0.0);
+      -webkit - transform: scale(0.0);
+} 50 % {
+  transform: scale(1.0);
+      -webkit - transform: scale(1.0);
+    }
+  }
+],
+})
+export class TableComponent {
+    @Input() showLoader : boolean;
+}
+`;
+const tableHtml1: string = `
+<!-- NOTE: For the componet to work , you need to wrap your
+component tempalate inside relative positioned div -->
+<div style="position : relative">
+    <spinner-component [spinnerShow]="show">
+    <div class="spinner-2">
+      <div class="double-bounce1"></div>
+      <div class="double-bounce2"></div>
+    </div>
+    </spinner-component>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Username</th>
+            </tr>
+        </thead>
+        <tbody>
             <tr *ngFor="#user of users">
                 <td>{{user.id}}
                 <td>{{user.firstname}}</td>
@@ -60,22 +142,31 @@ component tempalate inside relative positioned div -->
 `;
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './template/main.html',
+    selector: 'my-app',
+    templateUrl: './template/main.html',
 })
-export class AppComponent  { name = 'Angular';
-showLoader : boolean;
-appCode : string;
-tableCode : string;    
-    tableHtml : string;    
-constructor(){
-    this.appCode = appCode;
-this.tableCode = tableCode;
-    this.tableHtml = tableHtml;
-}
-fetch(): void{
-    this.showLoader = true;
-    setTimeout(()=>{this.showLoader=false},2000);
-    
-}
+export class AppComponent {
+    name = 'Angular';
+    showLoader: boolean;
+    showLoader1: boolean;
+    appCode: string;
+    tableCode: string;
+    tableCode1: string;
+    tableHtml: string;
+    tableHtml1: string;
+    constructor() {
+        this.appCode = appCode;
+        this.tableCode = tableCode;
+        this.tableCode1 = tableCode1;
+        this.tableHtml = tableHtml;
+        this.tableHtml1 = tableHtml1;
+    }
+    fetch(): void {
+        this.showLoader = true;
+
+        //  setTimeout(()=>{this.showLoader=false},2000);
+    }
+    fetchone(): void {
+        this.showLoader1 = true;
+    }
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -3,14 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
 import { TableWrapper }  from './table.wrapper.component';
+import { Table1Wrapper }  from './table1.wrapper.component';
 import { SpinnerComponentModule } from 'ng2-component-spinner';
 import 'prismjs/prism';
 import 'prismjs/components/prism-typescript';
 import { PrismComponent } from 'angular-prism';
 
 @NgModule({
-  imports:      [ BrowserModule ,SpinnerComponentModule],
-  declarations: [ AppComponent, TableWrapper,PrismComponent],
-  bootstrap:    [ AppComponent ]
+    imports: [BrowserModule, SpinnerComponentModule],
+    declarations: [AppComponent, TableWrapper, Table1Wrapper, PrismComponent],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
